c7568ecc1767da4067f073fb7190d6dda6c933c6
Irfan Suleman <ivsuleman@gmail.com>
2019-04-05 15:25:43 +0100

    Start with scrapit-app

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Route, Switch} from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Wasteitem from "./containers/Wasteitem";
@@ -10,18 +10,17 @@ import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import NewWasteItem from "./containers/NewWasteItem";
 
-export default (props, state) =>
-    <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/waste-items/new" exact component={Wasteitem} />
-        <Route path="/resident" exact component={Resident} />
-        <Route path="/collector" exact component={Collector} />
-        <Route path="/site" exact component={Site} />
-        <Route path="/signin" exact component={Login} />
-        <Route path="/signup" exact component={Signup} />
+export default (props, state) => (
+  <Switch>
+    <Route path="/" exact component={Home} />
+    <Route path="/waste-items/new" exact component={Wasteitem} />
+    <Route path="/resident" exact component={Resident} />
+    <Route path="/collector" exact component={Collector} />
+    <Route path="/site" exact component={Site} />
+    <Route path="/signin" exact component={Login} />
+    <Route path="/signup" exact component={Signup} />
 
-        {/* Finally, catch all unmatched routes */}
-        <Route component={NotFound} />
-
-    </Switch>;
-
+    {/* Finally, catch all unmatched routes */}
+    <Route component={NotFound} />
+  </Switch>
+);
